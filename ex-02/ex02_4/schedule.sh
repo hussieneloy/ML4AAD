a9c5b1cb9680468745e18ed3216c7ceb95608ba0
Mohammad-Ali Aerabi <angellandros@yahoo.com>
2018-05-23 21:38:40 +0200

    update the schedule

--- ex-02/ex02_4/schedule.sh
+++ ex-02/ex02_4/schedule.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #$ -q meta_core.q
-#$ -N ml4aad-benchmark
+#$ -N ml4aad-benchmark-10
 #$ -m ea
 #$ -v PYTHONPATH=:/home/arabim/mllab/venv/bin
 #$ -M arabim@informatik.uni-freiburg.de
-timeout 5000 /home/arabim/mllab/venv/bin/python /home/arabim/ML4AAD/ex-02/ex02_4/benchmark.py \
+timeout 172800 /home/arabim/mllab/venv/bin/python /home/arabim/ML4AAD/ex-02/ex02_4/benchmark.py \
     -p /home/arabim/mllab/venv/bin \
     -w /home/arabim/ML4AAD/ex-02/ex02_4/mllab \
-    -f /home/arabim/rawAllx1000.json \
-    -t 1 \
+    -f /home/arabim/mllab/cluster/rawAllx1000.json \
     -s 1 2 4 8 16 32 64 128 256 512 1024 \
     -i 1 \
-    -I 1
+    -I 10
