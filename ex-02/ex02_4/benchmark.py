e62e2b5acbfc77c3020d7402ec495b749bc7763e
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-06-05 00:16:10 +0200

    add roar

--- ex-02/ex02_4/benchmark.py
+++ ex-02/ex02_4/benchmark.py
@@ -27,6 +27,39 @@ def run_smac(python_path, w_dir, n_iter=5, input_file='../rawAllx1000.json', see
     return x, cost, smac
 
 
+def run_roar(python_path, w_dir, n_iter=5, input_file='../rawAllx1000.json', seeds=[1], task_ids=None, max_tries=10):
+
+    from smac.configspace import ConfigurationSpace
+    from ConfigSpace.hyperparameters import UniformFloatHyperparameter
+    from smac.scenario.scenario import Scenario
+    from smac.facade.roar_facade import ROAR
+
+    def test_func(cutoff):
+        result = find_cut_off.main(python_path=python_path, w_dir=w_dir, iter=n_iter, input_file=input_file,
+                                   cutoffs=[cutoff], seeds=seeds, task_ids=task_ids)
+        cleaned = [x[1] for x in result if 0.0 < x[1] < 1.0]
+        mean = np.mean(cleaned) if cleaned else 0.0
+        mean = mean if mean != 1.0 else 0.0
+        return 1.0 - mean
+
+    cs = ConfigurationSpace()
+    cutoff_parameter = UniformFloatHyperparameter('cutoff', 1.0, 99.0, default_value=50.0)
+    cs.add_hyperparameter(cutoff_parameter)
+    scenario = Scenario({"run_obj": "quality",   # we optimize quality (alternatively runtime)
+                         "runcount-limit": max_tries,  # maximum function evaluations
+                         "cs": cs,               # configuration space
+                         "deterministic": "true"
+                         })
+
+    roar = ROAR(scenario=scenario, tae_runner=test_func, rng=1234)
+
+    x = roar.optimize()
+
+    cost = test_func(x)
+
+    return x, cost, roar
+
+
 def plot(smac, plot=False):
     runhistory = smac.get_runhistory()
 
@@ -65,7 +98,8 @@ def clean_smac_shit():
 
 
 def main(args):
-    x, y, smac = run_smac(python_path=args.python_path, w_dir=args.working_dir, input_file=args.input_file,
+    run_func = run_smac if args.method == 'SMAC' else run_roar
+    x, y, smac = run_func(python_path=args.python_path, w_dir=args.working_dir, input_file=args.input_file,
                           n_iter=args.iter, seeds=args.seed, task_ids=args.tasks, max_tries=args.smac_iter)
     plot(smac)
     clean_smac_shit()
@@ -81,4 +115,5 @@ if __name__ == '__main__':
     calc_parser.add_argument('-f', '--input-file', default='rawAllx1000.json', help='the input json file')
     calc_parser.add_argument('-p', '--python-path', default='/usr/bin', type=str, help='absolute path of python exec')
     calc_parser.add_argument('-w', '--working-dir', default='.', type=str, help='path of working directory')
+    calc_parser.add_argument('-m', '--method', choices=['SMAC', 'ROAR'], default='SMAC')
     main(calc_parser.parse_args())
