9c1b569cdaaa165a4ef4e62132b71e3081bbf8cb
Saskia <rabalds@informatik.uni-freiburg.de>
2018-06-12 09:56:26 +0200

    added optimizer object

---
+++ ex-03/optimizer.py
@@ -0,0 +1,25 @@
+
+class ES(object):
+
+    """Interface that contains the main Evolutionary optimizer
+
+    Attributes:
+
+    """
+
+    def __init__(self):
+        pass
+
+    # Ideas for splitting functionality
+
+    def run(self):
+        pass
+
+    def get_parents(self):
+        pass
+
+    def cross_over(self, parent1, parent2):
+        pass
+
+    def racing(self, set_of_conf):
+        pass
