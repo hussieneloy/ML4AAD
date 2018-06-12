e3f581ce51669fcc013b832d3dd36d97a58d885d
Saskia <rabalds@informatik.uni-freiburg.de>
2018-06-12 16:14:33 +0200

    added initialization and __init__.py for importing optimizer

--- ex-03/optimizer.py
+++ ex-03/optimizer.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+from smac.configspace import Configuration
+from smac.intensification.intensification import Intensifier
+from smac.runhistory.runhistory import RunHistory
+from smac.scenario.scenario import Scenario
+from smac.utils.io.traj_logging import TrajLogger
+import ConfigSpace.util
+
 
 class ES(object):
 
@@ -7,13 +16,35 @@ class ES(object):
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 scenario: Scenario,
+                 runhistory: RunHistory,
+                 intensifier: Intensifier,
+                 aggregate_func: callable,
+                 rng: np.random.RandomState,
+                 restore_incumbent: Configuration=None):
+        
+        self.incumbent = restore_incumbent
+        self.scenario = scenario
+        self.confi_space = scenario.cs
+        self.runhistory = runhistory
+        self.intensifier = intensifier
+        self.aggregate_func = aggregate_func
+        self.rng = rng
 
     # Ideas for splitting functionality
 
     def run(self):
-        pass
+
+        conf1 = get_random_neighbour(self.scenario.cs.get_default_configuration())
+        conf2 = get_random_neighbour(self.scenario.cs.get_default_configuration())
+
+        print("Conf1: %s" % (conf1))
+        print("Conf2: %s" % (conf2))
+
+        self.race_configs([conf1, conf2])
+
+        print(self.incumbent)
 
     def get_parents(self):
         pass
@@ -21,5 +52,16 @@ class ES(object):
     def cross_over(self, parent1, parent2):
         pass
 
-    def racing(self, set_of_conf):
-        pass
+    def race_configs(self, set_of_conf):
+        """Races the challengers agains each other to determine incumbent
+
+        """
+        time_left = 5
+
+        self.incumbent, inc_perf = self.intensifier.intensify(
+            challengers=set_of_conf,
+            incumbent=self.incumbent,
+            run_history=self.runhistory,
+            aggregate_func=self.aggregate_func,
+            time_bound=max(self.intensifier._min_time, time_left)
+        )
